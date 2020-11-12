24af0a435b6cc90c92906692691fdb765c077193
bouromain <bouromain@gmail.com>
2020-11-12 13:17:28 +0000

    modification cemd, documentatio nand reference

--- hippocampy/stats/circ_stats.py
+++ hippocampy/stats/circ_stats.py
@@ -1,17 +1,31 @@
-# Code heavily inspired from matlab circ_stat toolbox and re-implementation 
-# by Raphael Vallat in the python package Pinguin
+# Code heavily inspired from matlab circ_stat toolbox and python package Pinguin
+# by Raphael Vallat. I also addded further function and helpers to work with 
+# circular variables 
+#
 # Provide helper function, descriptive statistics and statistical tests
 # for circular data
-# Reference:
+# 
+# References:
 # Berens, Philipp. 2009. CircStat: A MATLAB Toolbox for Circular Statistics.
 # Journal of Statistical Software, Articles 31 (10): 1–21.
 # Vallat, R. (2018). Pingouin: statistics in Python. Journal of Open Source 
 # Software, 3(31), 1026, https://doi.org/10.21105/joss.01026
-
+# 
+# Rabin, J., Delon, J. & Gousseau, Y. Transportation Distances on the Circle.
+# J Math Imaging Vis 41, 147 (2011). https://doi.org/10.1007/s10851-011-0284-0
+# https://arxiv.org/pdf/0906.5499v2.pdf
+#
+# J. Rabin, J. Delon and Y. Gousseau, "Circular Earth Mover’s Distance for the
+# comparison of local features," 2008 19th International Conference on Pattern
+# Recognition, Tampa, FL, 2008, pp. 1-4, doi: 10.1109/ICPR.2008.4761372.
+# http://citeseerx.ist.psu.edu/viewdoc/download?doi=10.1.1.214.4116&rep=rep1&type=pdf
+#
+# Author: RB 12/11/20
 
 import numpy as np
 import bottleneck as bn
 from numpy import pi
+
 ########################################################################
 ## Helpers
 ########################################################################
@@ -26,7 +40,6 @@ def isradians(x):
   else:
       # print("Warning, radians not in [-pi,pi] or [0,2pi] range")
       return 0  
-
 
 ########################################################################
 ## Descriptive statistics
@@ -45,6 +58,7 @@ def circ_mean(alpha, weight=None, dim=0):
     mu = np.angle(r)
 
     return mu
+
 
 def circ_r(alpha, weight=None , d=False , dim=0 ):
     '''
@@ -73,6 +87,7 @@ def circ_r(alpha, weight=None , d=False , dim=0 ):
 
 def circ_std(alpha, weight=None , d=False , dim=0 ):
     '''
+    Compute circular Standard Deviation
     '''
     alpha = np.asarray(alpha)
     if weight is None:
@@ -90,15 +105,13 @@ def circ_std(alpha, weight=None , d=False , dim=0 ):
     return s , s0
 
 
-
-
 ########################################################################
 ## Test statistics
 ########################################################################
 
 def corr_cl( x, theta, tail='two-sided'):
     '''
-    circular linear correlation between a circular variable theta and linear x
+    circular linear correlation between a linear variable x and circular theta 
     '''
     from scipy.stats import pearsonr, chi2
 
@@ -129,15 +142,20 @@ def corr_cl( x, theta, tail='two-sided'):
 ## other    
 ########################################################################
 
-def cemd(f,g, domain= 2*pi):
+def cemd(f,g, period=[0 , 2*pi] ):
     ''' 
     Calculate circular earth mover distance between two histograms
     of circular distributions 
 
+    Input: 
+            - f and g two histograms of circular distributions 
+            - period: is a variable defining on which domain we calculate the distance
+            (eg: [0, 2*pi] for radian, [0, 24] for hours, ....)
+    Output:
+            - CEMD: circular Wassertein distance
+
     References:
-    https://arxiv.org/pdf/0906.5499v2.pdf
-    https://perso.telecom-paristech.fr/gousseau/matching.pdf
-    http://citeseerx.ist.psu.edu/viewdoc/download?doi=10.1.1.214.4116&rep=rep1&type=pdf
+    Rabin et al 2011 
 
     see for potential non discret adaptation (around page 150)
     https://pastel.archives-ouvertes.fr/file/index/docid/472442/filename/these.pdf
@@ -172,8 +190,8 @@ def cemd(f,g, domain= 2*pi):
         # calculate sum of distances
         D[k] = bn.nansum( abs(F_k-G_k) )
     
-    # divide by n and multiply by "domain" in order to set the max distance 
-    # to this value
-    D = D / n * domain
+    # divide by n and multiply by "period" in order to set the max distance 
+    # to this value. 
+    D = D / n * np.diff(period)
 
     return bn.nanmin(D)
