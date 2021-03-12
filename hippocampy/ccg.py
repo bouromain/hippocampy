abdece488ee1108c3a6c719248922e7534562714
Romain <bouromain@gmail.com>
2021-03-12 10:09:19 +0000

    comment for ccg

--- hippocampy/ccg.py
+++ hippocampy/ccg.py
@@ -6,3 +6,12 @@ def ccg(spikes1, spikes2, binsize=1e-3, max_lag=250e-3):
     Compute cross-correlograms between two spike trains
     """
     print("to do")
+
+
+def continuous_ccg():
+    """
+    http://www.memming.com/codes/ccc.m.html
+    http://citeseerx.ist.psu.edu/viewdoc/download?doi=10.1.1.121.8458&rep=rep1&type=pdf
+
+    """
+    print("will compute ccg")
