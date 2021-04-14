f4e2866908867b9692f984ea0c9ff5b66f5b7045
bouromain <bouromain@gmail.com>
2021-04-14 19:47:36 +0100

    tif to h5 compression

--- hippocampy/io/IOtiff.py
+++ hippocampy/io/IOtiff.py
@@ -1,5 +1,7 @@
 from ScanImageTiffReader import ScanImageTiffReader
 import h5py
+import os
+import gc
 
 
 def get_tiff_metadata(pfile: str):
@@ -24,26 +26,76 @@ def get_tiff_data(pfile: str):
     return out
 
 
-# ## create a normal h5 file without compression
-# hfile = "/home/bouromain/Documents/tmpData/2020-03-13_12-20-06/m4368_20200313_00001.hdf5"
+pfile = "/home/bouromain/Documents/tmpData/compressionTest/m4466_20201203/m4453-20201203_00001.tif"
 
-# f = h5py.File(hfile)  # make an hdf5 file
-# dataTiff = get_tiff_data(pfile)
-# out = f.create_dataset('/x', data=dataTiff)
-# f.close()
 
-# ## create a normal h5 file without compression
-# gzipfile = "/home/bouromain/Documents/tmpData/2020-03-13_12-20-06/m4368_20200313_00001-gzip.hdf5"
+def tiff2h5(
+    pfile: str,
+    compression_type="gzip",
+    compression_lvl=5,
+    overwrite=False,
+    n_frblk=2000,
+):
+    """
+    convert a scanImage tiff file and convert is to a h5 file with compression
 
-# f = h5py.File(gzipfile)  # make an hdf5 file
-# dataTiff = get_tiff_data(pfile)
-# out = f.create_dataset('/x', data=dataTiff, compression="gzip", shuffle=True, chunks= True)
-# f.close()
+    Parameters:
+                - pfile: path of the file to process
+                - compression_type: type of compression ["gzip"(default), "lzf"]
+                - compression_lvl: level of compression
+                - overwrite: if should overwrite existing file
+                - n_frblk: number of frames to read per blocks
 
-# ## create a normal h5 file without compression
-# lzffile = "/home/bouromain/Documents/tmpData/2020-03-13_12-20-06/m4368_20200313_00001-lzf.hdf5"
+    TO DO:
 
-# f = h5py.File(lzffile)  # make an hdf5 file
-# dataTiff = get_tiff_data(pfile)
-# out = f.create_dataset('/x', data=dataTiff, compression="lzf", shuffle=True, chunks= True)
-# f.close()
+                - better handling of existing file eg erase it
+                - calculate nfr depending of available ram of the system
+                - store metadata of the tiff file
+    References:
+    https://vidriotech.gitlab.io/scanimagetiffreader-python
+    https://gitlab.com/vidriotech/scanimagetiffreader-python/-/blob/master/src/ScanImageTiffReader/__init__.py
+    https://docs.h5py.org/en/stable/high/dataset.html
+    """
+    # Check inputs
+    list_compression = ["gzip", "lzf"]
+    assert compression_type in list_compression, "Available compressions are gzip, lzf "
+    assert pfile.endswith(".tif"), "Input file should be a tiff file"
+
+    # prepare some things
+    h5_file_name = pfile.replace(".tif", ".h5")
+
+    if os.path.isfile(h5_file_name) and not overwrite:
+        raise FileExistsError("This file already exist, use overwrite=True")
+
+    # get shape of the tiff data [n_frames, pixels, pixels ]
+    tiff_shape = get_tiff_shape(pfile)
+
+    # open files for the loop
+    h5_f = h5py.File(h5_file_name)
+    tiff_f = ScanImageTiffReader(pfile)
+
+    # extract first frames and initiate a dataset with it
+    tmp_tiff_data = tiff_f.data(beg=0, end=n_frblk)
+    dset = h5_f.create_dataset(
+        "data",
+        data=tmp_tiff_data,
+        compression=compression_type,
+        compression_opts=compression_lvl,
+        shuffle=True,
+        chunks=True,
+        maxshape=(None, tiff_shape[1], tiff_shape[2]),
+    )
+
+    # now we can loop to incrementally fill the new h5 file
+    it_frame = n_frblk
+    while it_frame < tiff_shape[0]:
+
+        n_frblk = min(tiff_shape[0] - it_frame, n_frblk)
+
+        dset.resize(it_frame + n_frblk, axis=0)
+        tmp_tiff_data = tiff_f.data(beg=it_frame, end=it_frame + n_frblk)
+        dset[-n_frblk:, :, :] = tmp_tiff_data
+        it_frame += n_frblk
+
+    tiff_f.close()
+    h5_f.close()
