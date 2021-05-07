1e62584fb148728a23dd3aaafb4ae98f5f4b2ade
bouromain <bouromain@gmail.com>
2021-05-07 21:01:50 +0100

    some changes to fast CCG func

--- hippocampy/ccg.py
+++ hippocampy/ccg.py
@@ -1,5 +1,6 @@
 import numpy as np
 import bottleneck as bn
+from numba import jit
 
 
 def ccg(spikes1, spikes2, binsize=1e-3, max_lag=1000e-3):
@@ -29,77 +30,105 @@ def ccg(spikes1, spikes2, binsize=1e-3, max_lag=1000e-3):
     return c, E
 
 
-# from numba import jit
-# @jit(nopython=True)
-# def ccg2(spikes1, spikes2, binsize=1e-3, max_lag=1000e-3):
-#     """
-#     Fast crossCorr
-#     """
-#     # create edges and ensure that they are odd
-#     winsize_bins = 2 * int(max_lag / binsize)
+@jit(nopython=True)
+def ccg_heart(spikes1, spikes2, binsize=1e-3, max_lag=1000e-3):
+    """
+    Fast crosscorrelation code:
+    Assume that the spike trains are sorted
 
-#     if winsize_bins % 2 != 1:
-#         winsize_bins += 1
-#         max_lag += binsize / 2
+    i could potentially calculate the length of the spike
+    trains and loop over the smaller one. In this case the CCG
+    should be reversed at the end
 
-#     # Make edges (seem faster than np.linspace)
-#     E = np.zeros(winsize_bins + 1)
-#     for i in range(winsize_bins + 1):
-#         E[i] = -max_lag + i * binsize
-
-#     # initialise CCG
-#     C = np.zeros(winsize_bins - 1)
-#     # loop over spikes
-#     for i in spikes1:
-#         for j in spikes2:
-#             d = i - j
-#             if -max_lag < d < max_lag:
-#                 C[np.searchsorted(E, d, side="left") - 1] += 1
-#     return C, E
-
-
-def continuous_ccg(spikes1, spikes2, tau=10e-3, max_lag=100e-3):
+    Adapted from:
+    G. Viejo crossCorr function
+    M. Zugaro CCGEngine.c
     """
 
-    for now this function iis s simple translation of the following code:
-    http://www.memming.com/codes/ccc.m.html
-    http://citeseerx.ist.psu.edu/viewdoc/download?doi=10.1.1.121.8458&rep=rep1&type=pdf
+    sz1 = len(spikes1)
+    sz2 = len(spikes2)
+    # create edges and ensure that they are odd
+    winsize_bins = 2 * int(max_lag / binsize)
 
-    """
-    raise NotImplementedError("This should be tested")
+    if winsize_bins % 2 != 1:
+        winsize_bins += 1
+        max_lag += binsize / 2
 
-    spikes1 = np.random.uniform(0, 1, 100)
-    spikes2 = np.random.uniform(0, 1, 100)
+    halfbin = int(winsize_bins / 2) + 1
 
-    spikes1 = np.asarray(spikes1)
-    spikes2 = np.asarray(spikes2)
+    # Make edges (seem faster than np.linspace)
+    E = np.zeros(winsize_bins + 1)
+    for i in range(winsize_bins + 1):
+        E[i] = -max_lag + i * binsize
 
-    assert spikes1.size is not 0, "at least one spike is required for first spike train"
-    assert (
-        spikes2.size is not 0
-    ), "at least one spike is required for second spike train"
+    # initialise CCG
+    C = np.zeros(winsize_bins)
 
-    # calculate all the lags
-    delta_t = spikes1[None, :] - spikes2[:, None]
+    # loop over spikes
+    idx2 = 0
+    for idx1 in range(sz1):
+        # define the window around the spike of interest
+        l_bound = spikes1[idx1] - max_lag
+        H_bound = spikes1[idx1] + max_lag
 
-    # Only keep the ones that are smaller than max_lag
-    delta_t = np.ravel(delta_t)
-    m = np.abs(delta_t) <= max_lag
-    delta_t = delta_t[m]
+        # search for the max index in spike 2 in window:
+        while idx2 < sz2 and spikes2[idx2] < l_bound:
+            idx2 += 1
+        while idx2 > 1 and spikes2[idx2 - 1] > l_bound:
+            idx2 -= 1
+        # now we have this index we can accumulate value
+        # in the ccg as long as we are in the window
+        idx2_H = idx2
+        while idx2_H < sz2 and spikes2[idx2_H] < H_bound:
+            idx_C = halfbin + int(0.5 + (spikes2[idx2_H] - spikes1[idx1]) / binsize)
+            C[idx_C] += 1
+            idx2_H += 1
 
-    # sort the lags
-    delta_t = np.sort(delta_t)
+    return C, E
 
-    # Now do the little stuff I am not sure to understand
-    Q_plus = np.zeros_like(delta_t)
-    Q_minus = np.zeros_like(delta_t)
-    Q_minus[0] = 1
 
-    exp_delta = np.exp(-np.diff(delta_t) / tau)
-    N = delta_t.size
+# def continuous_ccg(spikes1, spikes2, tau=10e-3, max_lag=100e-3):
+#     """
 
-    for k in range(0, N - 1):
-        Q_minus[k] = 1 + Q_minus[k - 1] * exp_delta[k - 1]
-        Q_plus[-k + 2] = (Q_plus[-k + 1] + 1) * exp_delta[-k + 2]
+#     for now this function iis s simple translation of the following code:
+#     http://www.memming.com/codes/ccc.m.html
+#     http://citeseerx.ist.psu.edu/viewdoc/download?doi=10.1.1.121.8458&rep=rep1&type=pdf
 
-    return Q_minus + Q_plus, delta_t
+#     """
+#     raise NotImplementedError("This should be tested")
+
+#     spikes1 = np.random.uniform(0, 1, 100)
+#     spikes2 = np.random.uniform(0, 1, 100)
+
+#     spikes1 = np.asarray(spikes1)
+#     spikes2 = np.asarray(spikes2)
+
+#     assert spikes1.size is not 0, "at least one spike is required for first spike train"
+#     assert (
+#         spikes2.size is not 0
+#     ), "at least one spike is required for second spike train"
+
+#     # calculate all the lags
+#     delta_t = spikes1[None, :] - spikes2[:, None]
+
+#     # Only keep the ones that are smaller than max_lag
+#     delta_t = np.ravel(delta_t)
+#     m = np.abs(delta_t) <= max_lag
+#     delta_t = delta_t[m]
+
+#     # sort the lags
+#     delta_t = np.sort(delta_t)
+
+#     # Now do the little stuff I am not sure to understand
+#     Q_plus = np.zeros_like(delta_t)
+#     Q_minus = np.zeros_like(delta_t)
+#     Q_minus[0] = 1
+
+#     exp_delta = np.exp(-np.diff(delta_t) / tau)
+#     N = delta_t.size
+
+#     for k in range(0, N - 1):
+#         Q_minus[k] = 1 + Q_minus[k - 1] * exp_delta[k - 1]
+#         Q_plus[-k + 2] = (Q_plus[-k + 1] + 1) * exp_delta[-k + 2]
+
+#     return Q_minus + Q_plus, delta_t
