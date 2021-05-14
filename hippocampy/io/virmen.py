22a8b54885c3a8a3f0ea9974f9dc33caafc0bbe9
bouromain <bouromain@gmail.com>
2021-05-14 19:58:18 +0100

    add function to read .data files

---
+++ hippocampy/io/virmen.py
@@ -0,0 +1,40 @@
+import numpy as np
+import os
+
+
+def load_dat(path, n_vars=None, dtype="float"):
+    """
+    Open file in .data format produced by ViRMen
+
+    Parameters
+    ----------
+            - path: file path
+            - n_vars: number of column needed to reshape the data
+            - dtype: type of the data to read
+
+    Return
+    ------
+            - data: data as a numpy array
+    """
+
+    # to avoid problems
+    path = os.path.expanduser(path)
+
+    # check file exist
+    if not os.path.isfile(path):
+        raise FileNotFoundError
+
+    # now we can open the file safely
+    with open(path, "rb") as f:
+        data = np.fromfile(f, dtype=dtype)
+
+    n_sample = len(data) / n_vars
+
+    assert n_sample == np.floor(
+        n_sample
+    ), "number of sample in data is not divisible by n_vars "
+
+    if n_vars is not None:
+        data = np.reshape(data, (int(n_sample), n_vars))
+
+    return data
