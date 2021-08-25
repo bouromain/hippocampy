bd37cb71ff5da4b17b4006a9e029e96a16fccc5f
Romain <bouromain@gmail.com>
2021-08-25 10:30:09 +0100

    modification of spectral code

--- hippocampy/spectral.py
+++ hippocampy/spectral.py
@@ -1,30 +1,49 @@
 import numpy as np
-import ghostipy as gsp
+from scipy.signal import spectrogram as sp_spectrogram
+from scipy.fftpack import next_fast_len
+import pywt
+
+# import ghostipy as gsp
+# alternatively see ghostipy for mtspectrogram, cwt and synchrosqueezed
 
 ########################################################################
 ## Spectrogram
 ########################################################################
-def spectrogram(data, fs, *, bandwidth=15, nperseg=258, noverlap=16):
+def spectrogram(data, fs, *, nperseg=258, noverlap=16, axis=-1):
     """
     ...
     """
-    coefs, frequencies, timestamps = gsp.mtm_spectrogram(
-        data, fs=fs, bandwidth=bandwidth, nperseg=nperseg, noverlap=noverlap
+    l = data.shape[axis]
+    coefs, frequencies = sp_spectrogram(
+        data,
+        fs=fs,
+        nperseg=nperseg,
+        noverlap=noverlap,
+        axis=axis,
+        nfft=next_fast_len(l),
     )
 
-    return coefs, frequencies, timestamps
+    return coefs, frequencies
 
 
-def cwt_spectrogram(data, fs, *, freqs=[2, 250], axis=-1):
+def periods2scales(periods, wavelet=None, dt=1.0):
+    return (periods / dt) * pywt.central_frequency(wavelet)
+
+
+def freqs2scales(freqs, wavelet=None, dt=1.0):
+    return pywt.central_frequency(wavelet) / (freqs * dt)
+
+
+def cwt_spectrogram(data, freqs, Fs, *, wavelet="cmorl3-7", method="fft", axis=-1):
     """
-    Compute the wavelet transform of the input signal. 
+    Compute the wavelet transform of the input signal.
 
     Parameters
     ----------
     - data: array_like
         signal to be computed
-    - freqs: 
-
+    - freqs: array_like
+        array of frequency of interest for the continuous wavelet transform
     - fs: float
         Sampling frequency in Hertz
 
@@ -32,17 +51,25 @@ def cwt_spectrogram(data, fs, *, freqs=[2, 250], axis=-1):
     -------
 
     - coefs: array_like
-        Continuous wavelet transform of the input data for the given 
-        scales and wavelet. The first axis of coefs corresponds to the 
+        Continuous wavelet transform of the input data for the given
+        scales and wavelet. The first axis of coefs corresponds to the
         scales and the second is time.
-    
+
     - frequencies : array_like
         frequencies corresponding to the coefficients
 
-
-  """
+    References
+    ----------
+    https://github.com/alsauve/scaleogram
+    """
     if any(freqs <= 0):
         freqs[freqs <= 0] = 1
-    coefs, _, frequencies, timestamps, _ = gsp.cwt(data, fs=fs, freq_limits=freqs)
-    return np.abs(coefs), frequencies, timestamps
 
+    dt = 1 / Fs
+    scales = freqs2scales(freqs, wavelet=wavelet, dt=dt)
+
+    coefs, frequencies = pywt.cwt(
+        data, scales, wavelet=wavelet, sampling_period=dt, method=method, axis=axis
+    )
+
+    return np.abs(coefs), frequencies
