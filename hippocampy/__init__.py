174c08f2425a4d2dd0c4ac9eb0578a8a4a873045
Romain <bouromain@gmail.com>
2021-09-18 13:57:47 +0100

    fix utils location

--- hippocampy/__init__.py
+++ hippocampy/__init__.py
@@ -1,12 +1,13 @@
 from . import sig_tool
 from . import matrix_utils
 from . import calfunc
-from . import stats
-from . import io
-from . import model
 from . import ccg
 from . import spectral
 from . import wavelet
+
+from . import stats
+from . import model
+from . import io
 from . import utils
 
 # DataTypes
