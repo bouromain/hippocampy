42a545b36808d8772e1b630dffa543b4d67eb675
romain bourboulou <bouromain@gmail.com>
2021-10-11 11:50:37 +0100

    add smooth to rate map

--- hippocampy/binning.py
+++ hippocampy/binning.py
@@ -2,9 +2,20 @@ import bottleneck as bn
 import numpy as np
 
 from hippocampy.utils.type_utils import float_to_int
+from hippocampy.matrix_utils import smooth_1d
 
 
-def rate_map(var: np.ndarray, samples: np.ndarray, *, bins=10, method="spk"):
+def rate_map(
+    var: np.ndarray,
+    samples: np.ndarray,
+    *,
+    bins=10,
+    fs: int = 30,
+    smooth_half_win: int = 5,
+    smooth_axis: int = 0,
+    method="spk",
+    preserve_nan_opt=True
+):
     """
     rate_map [summary]
 
@@ -55,16 +66,27 @@ def rate_map(var: np.ndarray, samples: np.ndarray, *, bins=10, method="spk"):
     occ, _ = np.histogramdd(var, bins)
 
     # affect no occupancy to nan. This avoid zero division later
-    # and is necessary to consider non explored areas
+    # and is necessary to take into account non explored areas
     no_occ = occ == 0
     occ[no_occ] = np.nan
+    occ = occ / fs  # convert in Hz
 
     if method == "mean":
         act, _ = np.histogramdd(var, bins, weights=samples)
         act /= occ
-        rate = act
     elif method == "spk":
         act, _ = np.histogramdd(var[samples], bins)
-        rate = act / occ
 
-    return rate, act, occ
+    if smooth_half_win > 0:
+        act_s = smooth_1d(
+            act, smooth_half_win, axis=smooth_axis, preserve_nan_opt=preserve_nan_opt
+        )
+        occ_s = smooth_1d(
+            occ, smooth_half_win, axis=smooth_axis, preserve_nan_opt=preserve_nan_opt
+        )
+
+    if method == "spk":
+        rate_s = act_s / occ_s
+    elif method == "mean":
+        rate_s = act_s
+    return rate_s, act_s, occ_s
