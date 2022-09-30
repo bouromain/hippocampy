fix sign problem in cell in template function

hippocampy/assemblies.py
@@ -211,13 +211,19 @@ def cells_in_template(template: np.ndarray, n_std: float = 2.0):
     output: np.ndarray
         logical matrix telling if a cell is part of a template
     """
-    m = bn.nanmean(template, axis=0)
-    s = bn.nanstd(template, axis=0)
+    template_fixed = template.copy()
 
-    return np.logical_or(
-        template - m[None, :] > n_std * s[None, :],
-        template - m[None, :] < -n_std * s[None, :],
-    )
+    max = bn.nanmax(template, axis=0)
+    min = bn.nanmin(template, axis=0)
+
+    to_flip = np.abs(min) > max
+
+    template_fixed[:, to_flip] = -template[:, to_flip]
+
+    m = bn.nanmean(template_fixed, axis=0)
+    s = bn.nanstd(template_fixed, axis=0)
+
+    return template_fixed - m[None, :] > n_std * s[None, :]
 
 
 def sim_assemblies(

hippocampy/matrix_utils.py
@@ -300,6 +300,8 @@ def norm_axis(matrix: np.ndarray, method="max", axis=-1) -> np.ndarray:
             one: normalize such as the sum of the values per row/column
                 sums to one (eg: become a probability distribution)
             zscore: compute the row/column wise zscore
+            perc: normalise such that 0 = 5th percentile and 1 = 95th percentile 
+            of the original distribution
     axis : int, optional
         axis along which the function is performed, by default -1
 
@@ -309,7 +311,7 @@ def norm_axis(matrix: np.ndarray, method="max", axis=-1) -> np.ndarray:
         normalized matrix
 
     """
-    if method.lower() not in ["max", "one", "zscore"]:
+    if method.lower() not in ["max", "one", "zscore", "perc"]:
         raise ValueError(f"Method {method} not recognized")
 
     if method == "zscore":
@@ -320,6 +322,13 @@ def norm_axis(matrix: np.ndarray, method="max", axis=-1) -> np.ndarray:
     elif method == "max":
         divisor = bn.nanmax(matrix, axis=axis)
         off = bn.nanmin(matrix, axis=axis)
+        divisor -= off
+        return (matrix - np.expand_dims(off, axis=axis)) / np.expand_dims(
+            divisor, axis=axis
+        )
+    elif method == "perc":
+        divisor = np.nanpercentile(matrix, 95, axis=axis)
+        off = np.nanpercentile(matrix, 5, axis=axis)
         divisor -= off
         return (matrix - np.expand_dims(off, axis=axis)) / np.expand_dims(
             divisor, axis=axis